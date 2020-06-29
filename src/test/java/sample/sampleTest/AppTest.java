8c755845de61ebf2eae2e6c1a3ffb189abec11de
MANISHAGRAWAL <MANISHAGRAWAL@9.79.252.102>
2020-06-29 14:27:04 +0530

    hello

---
+++ src/test/java/sample/sampleTest/AppTest.java
@@ -0,0 +1,34 @@
+package sample.sampleTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void Scenario1() throws InterruptedException {
+	
+	// Open the Chrome Browser using ChromeDriver.exe
+	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+	
+	
+	WebDriver driver = new ChromeDriver();
+	
+	// Providing implicit wait of 5 seconds
+	driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+				
+	// Navigate to salesforce application
+	driver.navigate().to("https://login.salesforce.com/");
+	
+	driver.quit();
+}
+}
